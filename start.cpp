b230568d9ffb742c2f35c88b737e2e0bc70e5d6c
divyabiyani <divyabiyani26@gmail.com>
2016-11-03 21:44:53 +0530

    Input/Output Completed

---
+++ start.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include "function_str.cpp"
+
+using namespace std;
+
+int main()
+{
+	long long int noAperiodic,noPeriodic,i;
+
+	printf("Enter the no. of periodic tasks:");
+	scanf("%lld",&noPeriodic);
+	struct periodic periodicTasks[noPeriodic];
+	printf("Enter the details for %lld periodic tasks\n",noPeriodic);
+	for(i=0;i<noPeriodic;i++)
+	{
+		printf("Enter the computation time of periodic task %lld:",i+1);
+		scanf("%lld",&periodicTasks[i].capacity);
+		printf("Enter the time period of periodic task %lld:",i+1);
+		scanf("%lld",&periodicTasks[i].timePeriod);
+	}
+
+	printf("Enter the no. of Aperiodic requests:");
+	scanf("%lld",&noAperiodic);
+	struct aperiodic aperiodicTasks[noAperiodic];
+	printf("Enter the details for %lld aperiodic requests\n",noAperiodic);
+	for(i=0;i<noAperiodic;i++)
+	{
+		printf("Enter the arrival time of aperiodic request %lld:",i+1);
+		scanf("%lld",&aperiodicTasks[i].arrivalTime);
+		printf("Enter the computation time of aperiod request %lld:",i+1);
+		scanf("%lld",&periodicTasks[i].capacity);
+	}
+
+	return 0;
+}
